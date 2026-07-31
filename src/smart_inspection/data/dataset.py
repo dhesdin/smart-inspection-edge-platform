2c4d0a2ce7009f6131a46980ad67c92c7f9d48d0
Valentin Dhesdin <185408513+Soewyth@users.noreply.github.com>
2026-07-31 22:37:29 +0200

    feat(ci): add minimal GItHub Action lint workflows Add ruff check and format but no test yet (empty)
    Add __init__ anomaly class, need to implement the private discover sample method

--- src/smart_inspection/data/dataset.py
+++ src/smart_inspection/data/dataset.py
@@ -1,8 +1,26 @@
+from smart_inspection.config.loader import resolve_config_paths
+
+
 class AnomalyDataset:
-    def __init__(self):
+    def __init__(self, category: str, split: str):
+        self.category = category
+        self.split = split
+        
+        # get paths
+        config = resolve_config_paths(config_path="common.yaml")
+        dataset_path = config["data"]["datasets"]
+        category_path = dataset_path / category
+        split_path = category_path / split
+
+        if not split_path.exists():
+            raise FileNotFoundError(f"Split path not found at {split_path} it should be called 'train' or 'test'")
+        
+        self.samples = self._discover_samples(split_path)  # Pass the split_path to the method
+
+
         pass  # TODO : Implement the logic
 
-    def _discover_samples(self, samples: dict) -> list:
+    def _discover_samples(self, split_path) -> list:
         pass  # TODO : Implement the logic
 
     def __len__(self) -> int:
